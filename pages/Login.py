fbd35b5065766334b45536941fd2b82a02de76aa
Tugce Usta <tugce.gulsan@gmail.com>
2024-10-29 16:29:25 +0300

    route different pages depending on the user role.

--- pages/Login.py
+++ pages/Login.py
@@ -3,17 +3,35 @@ import sqlite3
 
 conn = sqlite3.connect('gradPortal.sqlite3')
 c= conn.cursor()
-st.subheader('Welcome to Hacettepe Graduation Portal')
+st.subheader('Welcome to Hacettepe Graduation Portal!')
+
+if 'logged_in' not in st.session_state:
+    st.session_state.logged_in = False
 
 def login():
-    with st.form("Login", clear_on_submit=True):
-        email = st.text_input("Email")
-        password = st.text_input("Password")
-        if st.form_submit_button("Login"):
-            search_user = c.execute("SELECT * FROM Users WHERE email = ? AND password = ? ", (email, password) ).fetchone()
-            if search_user == None:
+    # Eğer kullanıcı giriş yapmamışsa login formunu gösterir
+    if not st.session_state.logged_in:
+        with st.form("Login", clear_on_submit=True):
+            email = st.text_input("Email")
+            password = st.text_input("Password", type="password")
+            submitted = st.form_submit_button("Login")
+
+        # Form submit edildiğinde
+        if submitted:
+            search_user = c.execute("SELECT * FROM Users WHERE email = ? AND password = ?", (email, password)).fetchone()
+            if search_user is None:
                 st.error("Invalid Email or Password")
             else:
-                pages = st.navigation([st.Page("pages/Education.py"), st.Page("pages/Job.py"), st.Page("pages/PersonalInfo.py")])
-                pages.run()
+                st.session_state.logged_in = True
+                st.success("Logged in successfully")
+                st.session_state.role = search_user[6]
+                st.rerun()  # Sayfayı yenilemek için
+    else:
+        if st.session_state.role == 'admin':
+            pages = st.navigation([st.Page("pages/Admin.py")])
+            pages.run()
+        else:
+            pages = st.navigation([st.Page("pages/Education.py"), st.Page("pages/Job.py"), st.Page("pages/PersonalInfo.py"), st.Page("pages/Logout.py")])
+            pages.run()
+
 login()
